ecf78332fbb4af860740cf4d3e4d17da43e06ab1
orbitz <orbitz@gmail.com>
2013-04-01 23:12:11 +0200

    Refactor code to be in modules and cleaned up a bit

--- lib/forbin/forbin.ml
+++ lib/forbin/forbin.ml
@@ -9,36 +9,99 @@ type t = { nick        : string
 	 ; ctl         : string
 	 }
 
-let safe f =
-  try
-    Some (f ())
-  with
-    | _ ->
-      None
+module Safe = struct
+  let safe f =
+    try
+      Some (f ())
+    with
+      | _ ->
+	None
 
-let safe_input_line in_chan =
-  safe (fun () -> input_line in_chan)
+  let input_line in_chan =
+    safe (fun () -> input_line in_chan)
 
-let safe_open_in path =
-  safe (fun () -> open_in path)
+  let open_in path =
+    safe (fun () -> open_in path)
 
-let safe_index s ch =
-  safe (fun () -> String.index s ch)
+  let index s ch =
+    safe (fun () -> String.index s ch)
 
-let safe_re_exec re s =
-  safe (fun () -> Re.get_all (Re.exec re s))
+  let re_exec re s =
+    safe (fun () -> Re.get_all (Re.exec re s))
 
-let safe_map_get k db =
-  safe (fun () -> String_map.find k db)
+  let map_get k db =
+    safe (fun () -> String_map.find k db)
 
-let lsplit2 ~on s =
-  match safe_index s on with
-    | Some idx ->
-      let l = String.sub s 0 idx in
-      let r = String.sub s (idx + 1) (String.length s - idx - 1) in
-      Some (l, r)
-    | None ->
-      None
+  let lsplit2 ~on s =
+    match index s on with
+      | Some idx ->
+	let l = String.sub s 0 idx in
+	let r = String.sub s (idx + 1) (String.length s - idx - 1) in
+	Some (l, r)
+      | None ->
+	None
+end
+
+module Word = struct
+  let take s = Safe.lsplit2 ~on:' ' s
+
+  let rec drop n s =
+    match n with
+      | n when n < 1 ->
+	None
+      | 1 ->
+	Some s
+      | n -> begin
+	match take s with
+	  | Some (_, rest) ->
+	    drop (n - 1) rest
+	  | None ->
+	    None
+      end
+end
+
+module Ctl = struct
+  let cmd ctl c =
+    let fout = open_out_gen [Open_append] 0o666 ctl in
+    output_string fout (c ^ "\n");
+    close_out fout
+
+  let send_msg t dst msg = cmd t.ctl ("MSG " ^ dst ^ " " ^ msg)
+end
+
+module Db = struct
+  let rec read_db in_chan db =
+    match Safe.input_line in_chan with
+      | Some l -> begin
+	match Safe.lsplit2 ~on:'\t' l with
+	  | Some (name, value) ->
+	    read_db
+	      in_chan
+	      (String_map.add name value db)
+	  | None ->
+	    read_db in_chan db
+      end
+      | None ->
+	db
+
+  let read_db_if_exists path =
+    match Safe.open_in path with
+      | Some in_chan -> begin
+	let ret = read_db in_chan String_map.empty in
+	close_in in_chan;
+	ret
+      end
+      | None ->
+	String_map.empty
+
+  let write_db db db_path =
+    let fout = open_out db_path in
+    String_map.iter
+      (fun k v ->
+	Printf.fprintf fout "%s\t%s\n" k v)
+      db;
+    close_out fout
+end
 
 let make_set_factoid_re nick =
   Re.compile
@@ -66,58 +129,20 @@ let make_get_factoid_re nick =
        ; Re.group (Re.rep Re.any)
        ])
 
-let cmd ctl c =
-  let fout = open_out_gen [Open_append] 0o666 ctl in
-  output_string fout (c ^ "\n");
-  close_out fout
-
-let send_msg t dst msg = cmd t.ctl ("MSG " ^ dst ^ " " ^ msg)
-
-let rec read_db in_chan db =
-  match safe_input_line in_chan with
-    | Some l -> begin
-      match lsplit2 ~on:'\t' l with
-	| Some (name, value) ->
-	  read_db
-	    in_chan
-	    (String_map.add name value db)
-	| None ->
-	  read_db in_chan db
-    end
-    | None ->
-      db
-
-let read_db_if_exists path =
-  match safe_open_in path with
-    | Some in_chan -> begin
-      let ret = read_db in_chan String_map.empty in
-      close_in in_chan;
-      ret
-    end
-    | None ->
-      String_map.empty
-
-let write_db db db_path =
-  let fout = open_out db_path in
-  String_map.iter
-    (fun k v ->
-      Printf.fprintf fout "%s\t%s\n" k v)
-    db;
-  close_out fout
 
 let set_factoid factoid value t =
   let t = { t with db = String_map.add factoid value t.db } in
-  write_db t.db t.db_path;
+  Db.write_db t.db t.db_path;
   t
 
 let parse_msg msg t =
-  match safe_re_exec t.set_factoid msg with
+  match Safe.re_exec t.set_factoid msg with
     | Some [| _; factoid; "is reply"; value |] ->
       Some (`Set (factoid, value))
     | Some [| _; factoid; "is"; value |] ->
       Some (`Set (factoid, factoid ^ " is " ^ value))
     | _ -> begin
-      match safe_re_exec t.get_factoid msg with
+      match Safe.re_exec t.get_factoid msg with
 	| Some [| _; factoid; args |] ->
 	  Some (`Get (factoid, args))
 	| _ ->
@@ -125,9 +150,9 @@ let parse_msg msg t =
     end
 
 let parse_args args =
-  match lsplit2 ~on:' ' args with
+  match Safe.lsplit2 ~on:' ' args with
     | Some (src, rest) -> begin
-      match lsplit2 ~on:' ' rest with
+      match Safe.lsplit2 ~on:' ' rest with
 	| Some (dst, msg) ->
 	  Some (src, dst, msg)
 	| None ->
@@ -144,20 +169,20 @@ let update_nick nick t =
   }
 
 let get_factoid factoid args db =
-  match safe_map_get factoid db with
+  match Safe.map_get factoid db with
     | Some v ->
       Some v
     | None ->
       None
 
 let rec loop t =
-  match safe_input_line t.in_chan with
+  match Safe.input_line t.in_chan with
     | Some l ->
       eval_input l t
     | None ->
       ()
 and eval_input line t =
-  match lsplit2 ~on:' ' line with
+  match Safe.lsplit2 ~on:' ' line with
     | Some ("MSG", args) ->
       eval_msg args t
     | Some ("WHOAMI", nick) ->
@@ -183,13 +208,13 @@ and interpret_msg src dst msg t =
   match parse_msg msg t with
     | Some (`Set (factoid, value)) -> begin
       let t = set_factoid factoid value t in
-      send_msg t dst (factoid ^ " set");
+      Ctl.send_msg t dst (factoid ^ " set");
       loop t
     end
     | Some (`Get (factoid, args)) -> begin
       match get_factoid factoid args t.db with
 	| Some resp -> begin
-	  cmd t.ctl (Printf.sprintf "MSG %s %s, %s" dst src resp);
+	  Ctl.send_msg t dst (Printf.sprintf "%s, %s" src resp);
 	  loop t
 	end
 	| None ->
@@ -200,9 +225,9 @@ and interpret_msg src dst msg t =
 
 let start ~ctl ~in_chan ~db_path =
   let rec read_whoami in_chan =
-    match safe_input_line in_chan with
+    match Safe.input_line in_chan with
       | Some l -> begin
-	match lsplit2 ~on:' ' l with
+	match Safe.lsplit2 ~on:' ' l with
 	  | Some ("WHOAMI", nick) ->
 	    nick
 	  | _ ->
@@ -211,7 +236,7 @@ let start ~ctl ~in_chan ~db_path =
       | None ->
 	failwith "ARGH"
   in
-  cmd ctl "WHOAMI";
+  Ctl.cmd ctl "WHOAMI";
   let nick = read_whoami in_chan in
   loop
     { nick        = nick
@@ -220,7 +245,7 @@ let start ~ctl ~in_chan ~db_path =
     ; ctl         = ctl
     ; in_chan     = stdin
     ; db_path     = db_path
-    ; db          = read_db_if_exists db_path
+    ; db          = Db.read_db_if_exists db_path
     }
 
 let main () =
